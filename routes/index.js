ee7f2c75c251a72a4910d39f6ae36e7c27468c74
Hyungsoo Kim <kim@biocourt.com>
2018-05-06 18:01:16 +0900

    add cdp render

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tetration Application Dependency Diagram' });
 });
 
+router.get('/cdp',function(req, res, next) {
+  res.render('cdp', { title: 'CDP Test ' });
+});
+
 module.exports = router;
